10d1f2f39a5b81b624cc5f7cfbf6b37f467780df
Peyton <peyton.braun@gmail.com>
2026-07-14 09:35:35 -0700

    Update grey_flag_alert.py

--- grey_flag_alert.py
+++ grey_flag_alert.py
@@ -3,6 +3,7 @@ from resuspension_alert.gfs import check_wind
 from resuspension_alert.snow import check_snow
 from resuspension_alert.soil_moisture import check_soil_moisture
 from resuspension_alert.email import send_email
+from resuspension_alert.github_state import (alert_exists, create_alert_issue, close_alert_issue)
 
 def send_alert():
     run_date = datetime.now(timezone.utc)
@@ -12,9 +13,15 @@ def send_alert():
     wind_ok = check_wind(model_date)
     snow_ok = check_snow(run_date)
     soil_moisture_ok = check_soil_moisture(model_date)
-    
-    if wind_ok[0] and snow_ok and soil_moisture_ok[0]:
-        send_email(date)
+
+    alert = wind_ok[0] and snow_ok and soil_moisture_ok[0]
+
+    if alert:
+        if not alert_exists():
+            send_email(run_date)
+            create_alert_issue(f"""Grey Flag Alert""")
+    else:
+        close_alert_issue()
 
 if __name__ == "__main__":
     send_alert()
