b7444664158f57195393e5b5fdcdbd54b1459387
Peyton <peyton.braun@gmail.com>
2026-07-14 11:12:27 -0700

    Update snow.py
    Updated to take latest snotel data point.

--- resuspension_alert/snow.py
+++ resuspension_alert/snow.py
@@ -1,18 +1,19 @@
 import requests
 import pandas as pd
-from datetime import datetime
+from datetime import datetime, timedelta, timezone
 
-def check_snow(date):
+def check_snow():
     '''Checks whether the Spirit Lake SNOTEL station had any snow on the selected date. Returns True if there is NO snow, False if there IS snow. Date must be input as a datetime.'''
-    date = date.strftime("%Y-%m-%d")
+    end_date = datetime.now(timezone.utc)
+    start_date = end_date - timedelta(days=7)
     url = 'https://wcc.sc.egov.usda.gov/awdbRestApi/services/v1/data'
     station_ID = '777:WA:SNTL'
     params = {
         "stationTriplets": station_ID,
         "elements": "SNWD",
         "duration": "DAILY",
-        "beginDate": date,
-        "endDate": date,
+        "beginDate": start_date.strftime("%Y-%m-%d"),
+        "endDate": end_date.strftime("%Y-%m-%d"),
         "periodRef": "END",
         "returnFlags": "false",
         "returnOriginalValues": "false",
@@ -24,7 +25,13 @@ def check_snow(date):
         raise Exception(f"API error: {response.status_code}")
 
     data = response.json()
-    snow_depth = data[0]["data"][0]["values"][0]["value"]
+    observations = data[0]["data"]
+    observations = [
+        obs for obs in observations
+        if obs["values"][0]["value"] is not None
+        ]
+
+    snow_depth = observations[-1]["values"][0]["value"]
 
     if snow_depth == 0:
         return True
