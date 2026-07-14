ecbb7b7b145a97c4ef608bea0052cc6324b96773
Peyton <peyton.braun@gmail.com>
2026-07-14 10:06:07 -0700

    Update gfs.py
    Updated to forecast hour 2.

--- resuspension_alert/gfs.py
+++ resuspension_alert/gfs.py
@@ -7,7 +7,7 @@ from datetime import datetime
 
 def get_gfs_data(date):
     '''Gets GFS model data based on a datetime input. Returns a function in the variable ds.'''
-    H = Herbie(date, model="gfs", save_dir="/AshResuspension", overwrite=False, verbose=False, fxx=1)
+    H = Herbie(date, model="gfs", save_dir="/AshResuspension", overwrite=False, verbose=False, fxx=2)
     ds = H.xarray(r":(?:UGRD|VGRD):(?:850|825|800|775|750) mb:").sel(latitude=46.15, longitude=238, method='nearest')
     ds = ds.assign(wind_speed=np.sqrt(ds.u**2 + ds.v**2))
     ds = ds.assign(wind_direction=(270 - np.degrees(np.arctan2(ds.v, ds.u))) % 360)
