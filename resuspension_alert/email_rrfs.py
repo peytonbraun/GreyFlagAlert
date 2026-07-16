cc8ddfda763b7f355c4557cf1e368ad1350223c5
Peyton <peyton.braun@gmail.com>
2026-07-16 07:50:50 -0700

    Create email_rrfs.py

---
+++ resuspension_alert/email_rrfs.py
@@ -0,0 +1,39 @@
+from resuspension_alert.rrfs import check_wind
+from resuspension_alert.snow import check_snow
+from resuspension_alert.soil_moisture import check_soil_moisture
+import smtplib
+from email.message import EmailMessage
+from datetime import datetime, timezone, timedelta
+import os
+
+def send_email(date):
+    run_date = date.replace(tzinfo=None)
+    model_date = (run_date.replace(minute=0, second=0, microsecond=0)- timedelta(hours=2)).replace(tzinfo=None)
+    wind_ok = check_wind(model_date)
+    snow_ok = check_snow()
+    soil_moisture_ok = check_soil_moisture(model_date)
+    
+    sender = os.environ['EMAIL_ADDRESS']
+    recipient = os.environ['RECIPIENT_EMAIL']
+    app_password = os.environ['EMAIL_PASSWORD']
+
+    msg = EmailMessage()
+    msg["Subject"] = "Gray Flag Alert - RRFS"
+    msg["From"] = sender
+    msg["To"] = recipient
+    
+    msg.set_content(f"""
+    Conditions for resuspension met on {run_date} Z.
+    
+    Wind speed = {wind_ok[1]} knots
+    Wind direction = {wind_ok[2]} degrees
+    Wind level = {wind_ok[3]} mb
+    Snow depth =  0 in
+    Soil moisture = {soil_moisture_ok[1]}%
+    
+    MSH webcam: https://volcview.wr.usgs.gov/ashcam-gui/webcam.html?webcam=msh-edifice
+    """)
+    
+    with smtplib.SMTP_SSL("smtp.gmail.com", 465) as smtp:
+        smtp.login(sender, app_password)
+        smtp.send_message(msg)
